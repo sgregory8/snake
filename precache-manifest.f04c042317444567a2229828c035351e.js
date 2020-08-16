25d24fa312c492852e6b145fb6aa1e255565a1bc
Gregory, Sam (ELS-OXF) <s.gregory@elsevier.com>
2020-08-16 19:08:06 +0100

    Updates

---
+++ precache-manifest.f04c042317444567a2229828c035351e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0942b646fbaa8cfa55910d2217d12c4",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "e31742a2aa45e5935aa5",
+    "url": "/snake/static/css/main.b0f07e29.chunk.css"
+  },
+  {
+    "revision": "0e908bf761d6ffc0dd91",
+    "url": "/snake/static/js/2.268c85dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.268c85dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31742a2aa45e5935aa5",
+    "url": "/snake/static/js/main.cc1bf610.chunk.js"
+  },
+  {
+    "revision": "330302f1424891b30bb2",
+    "url": "/snake/static/js/runtime-main.f5fb9ecf.js"
+  }
+]);
